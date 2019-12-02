7b9281ef9f8bc192eddb4bad4f61afd5e4b42ed4
auduongtansang <1712145@student.hcmus.edu.vn>
2019-12-02 13:15:21 +0700

    Shapes

--- TheHands3D/Form1.cs
+++ TheHands3D/Form1.cs
@@ -9,7 +9,9 @@ namespace TheHands3D
 	public partial class mainForm : Form
 	{
 		Camera camera = new Camera();
-		Shape cube = new Shape(Shape.ShapeType.CUBE);
+		Shape cube = new Shape(Shape.ShapeType.CUBE, Color.White);
+		Shape pyramid = new Shape(Shape.ShapeType.PYRAMID, Color.Red);
+		Shape prismatic = new Shape(Shape.ShapeType.PRISMATIC, Color.Aqua);
 
 		public mainForm()
 		{
@@ -84,8 +86,10 @@ namespace TheHands3D
 			gl.End();
 			gl.LineWidth(1.0f);
 
-			//Vẽ khối lập phương
+			//Vẽ các khối
 			cube.Draw(gl);
+			pyramid.Draw(gl);
+			prismatic.Draw(gl);
 
 			gl.Flush();
 		}
@@ -143,51 +147,114 @@ namespace TheHands3D
 
 		//Loại hình vẽ
 		ShapeType type;
-
 		//Màu hình vẽ
 		public Color color;
 
+		//Tâm đáy
+		public Tuple<double, double, double> center;
+		//Chiều cao
+		public double h;
+		//Chiều dài cạnh đáy
+		public double a;
+
 		//Tập đỉnh
 		public List<Tuple<double, double, double>> vertex;
-
 		//Tập thứ tự các đỉnh vẽ
 		public List<int> index;
 
-		public Shape(ShapeType userType)
+		public Shape(ShapeType userType, Color userColor)
 		{
+			a = h = 2;
 			type = userType;
-			color = Color.White;
+			color = userColor;
 
 			if (type == ShapeType.CUBE)
 			{
 				//Khối lập phương, 8 đỉnh
+				center = new Tuple<double, double, double>(1, 1, 0);
+
 				vertex = new List<Tuple<double, double, double>>();
 				vertex.Add(new Tuple<double, double, double>(0, 0, 0));
 				vertex.Add(new Tuple<double, double, double>(0, 2, 0));
 				vertex.Add(new Tuple<double, double, double>(2, 2, 0));
 				vertex.Add(new Tuple<double, double, double>(2, 0, 0));
-				vertex.Add(new Tuple<double, double, double>(0, 0, 2));
-				vertex.Add(new Tuple<double, double, double>(0, 2, 2));
-				vertex.Add(new Tuple<double, double, double>(2, 2, 2));
-				vertex.Add(new Tuple<double, double, double>(2, 0, 2));
+				vertex.Add(new Tuple<double, double, double>(0, 0, h));
+				vertex.Add(new Tuple<double, double, double>(0, 2, h));
+				vertex.Add(new Tuple<double, double, double>(2, 2, h));
+				vertex.Add(new Tuple<double, double, double>(2, 0, h));
 
 				//Thứ tự vẽ
 				index = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 5, 4, 2, 3, 7, 6, 1, 2, 6, 5, 0, 3, 7, 4 };
+			}
+			else if (type == ShapeType.PYRAMID)
+			{
+				//Khối chóp tứ giác, 5 đỉnh
+				center = new Tuple<double, double, double>(1, 3, 0);
+
+				vertex = new List<Tuple<double, double, double>>();
+				vertex.Add(new Tuple<double, double, double>(1, 3, h));
+				vertex.Add(new Tuple<double, double, double>(0, 2, 0));
+				vertex.Add(new Tuple<double, double, double>(0, 4, 0));
+				vertex.Add(new Tuple<double, double, double>(2, 4, 0));
+				vertex.Add(new Tuple<double, double, double>(2, 2, 0));
+
+				//Thứ tự vẽ
+				index = new List<int>() { 1, 2, 3, 4, 0, 1, 2, 0, 2, 3, 0, 3, 4, 0, 4, 1 };
+			}
+			else if (type == ShapeType.PRISMATIC)
+			{
+				//Khối lăng trụ, 6 đỉnh
+				center = new Tuple<double, double, double>(3, a * Math.Sqrt(3) / 6, 0);
+
+				vertex = new List<Tuple<double, double, double>>();
+				vertex.Add(new Tuple<double, double, double>(2, 0, 0));
+				vertex.Add(new Tuple<double, double, double>(3, 2 * Math.Sqrt(3) / 2, 0));
+				vertex.Add(new Tuple<double, double, double>(4, 0, 0));
+				vertex.Add(new Tuple<double, double, double>(2, 0, h));
+				vertex.Add(new Tuple<double, double, double>(3, 2 * Math.Sqrt(3) / 2, h));
+				vertex.Add(new Tuple<double, double, double>(4, 0, h));
+
+				//Thứ tự vẽ
+				index = new List<int>() { 0, 1, 2, 3, 4, 5, 0, 1, 4, 3, 1, 2, 5, 4, 0, 2, 5, 3};
 			}
 		}
 
 		public void Draw(OpenGL gl)
 		{
+			gl.Color(color.R, color.G, color.B, (byte)(50));
+
 			if (type == ShapeType.CUBE)
 			{
-				//Vẽ khối lập phương
-				gl.Color(color.R, color.G, color.B, (byte)(50));
+				//Vẽ khối lập phương (các mặt chữ nhật)
+				gl.Begin(OpenGL.GL_QUADS);
+					for (int i = 0; i < index.Count; i++)
+						gl.Vertex(vertex[index[i]].Item1, vertex[index[i]].Item2, vertex[index[i]].Item3);
+				gl.End();
+			}
+			else if (type == ShapeType.PYRAMID)
+			{
+				//Vẽ khối chóp (mặt đáy chữ nhật, các mặt bên tam giác)
+				gl.Begin(OpenGL.GL_QUADS);
+				for (int i = 0; i < 4; i++)
+					gl.Vertex(vertex[index[i]].Item1, vertex[index[i]].Item2, vertex[index[i]].Item3);
+				gl.End();
+
+				gl.Begin(OpenGL.GL_TRIANGLES);
+					for (int i = 4; i < index.Count; i++)
+						gl.Vertex(vertex[index[i]].Item1, vertex[index[i]].Item2, vertex[index[i]].Item3);
+				gl.End();
+			}
+			else if (type == ShapeType.PRISMATIC)
+			{
+				//Vẽ khối lăng trụ (mặt đáy tam giác, các mặt bên chữ nhật)
+				gl.Begin(OpenGL.GL_TRIANGLES);
+					for (int i = 0; i < 6; i++)
+						gl.Vertex(vertex[index[i]].Item1, vertex[index[i]].Item2, vertex[index[i]].Item3);
+				gl.End();
 
 				gl.Begin(OpenGL.GL_QUADS);
-
-				for (int i = 0; i < index.Count; i++)
+				for (int i = 6; i < index.Count; i++)
 					gl.Vertex(vertex[index[i]].Item1, vertex[index[i]].Item2, vertex[index[i]].Item3);
-
 				gl.End();
 			}
 		}
